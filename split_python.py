6be38f78e0790df4f024de71e72c1fa8d1fc5c4b
Vicky-analyst <azubikevictory72@gmail.com>
2025-09-19 15:34:33 -0700

    adding new codes to my script

--- split_python.py
+++ split_python.py
@@ -1,9 +1,13 @@
 print("Hello World")
 my_list = []
+count = 0
 while True:
     my_input = input("Enter input or complete to end loop: ")
     if my_input == "complete":
         break
     else:
         my_list.append(int(my_input))
-print(my_list)
+for i in my_list:
+    count += 1
+print(" ")
+print(my_list, count)
